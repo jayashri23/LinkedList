ed4a4265d8beb7501b04b42fef98788588323922
jayashri23 <jsjankar23@gmail.com>
2022-11-11 11:28:42 +0530

    Adding elements  in linked list at last

---
+++ src/com/linkedlist/AddAtLast.java
@@ -0,0 +1,59 @@
+package com.linkedlist;
+
+public class AddAtLast {
+
+    static class Node {
+        int data;
+        AddLinkedList.Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    AddLinkedList.Node head = null;
+    AddLinkedList.Node tail = null;
+
+    //adding to linked list
+    public void addToLinkedList(int data) {
+        AddLinkedList.Node newNode = new AddLinkedList.Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else
+        {
+            newNode.next = head;
+            head= newNode;
+            tail.next = null;
+            tail.next = newNode;
+            newNode.next = null;
+        }
+        tail= newNode;
+    }
+    //print linkedList
+    public void printLinkedList ()
+    {
+        AddLinkedList.Node current = head;
+        while (current != null)
+        {
+            System.out.println(current.data + "");
+            current = current.next;
+        }
+    }
+    public static void main (String[] args)
+    {
+        System.out.println("...............................");
+        System.out.println("Welcome to Linked list Program ");
+        System.out.println("...............................");
+        LinkedList linkedList = new LinkedList();
+        linkedList.addToLinkedList(70);
+        linkedList.addToLinkedList(30);
+        linkedList.addToLinkedList(56);
+        linkedList.addToLinkedList(56);  //56 is added first
+        linkedList.addToLinkedList(30);  //30 is added after 56
+        linkedList.addToLinkedList(70);     //70 is added after 30
+        linkedList.printLinkedList();
+    }
+}
+
+
+
